a1a47a2db9b7148d33ebdfd60adb558f9374ae1b
ruifarfetch <rui.nunes@farfetch.com>
2015-02-20 10:40:54 +0000

    up

--- static/js/modules/bxslider/sliderArticle.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
- 
-	$('.sliderSingle').bxSlider({
-	  mode: 'fade',
-	  controls:'false'
-	});
-
-});
--- static/js/modules/bxslider/sliderCarouselWishlist.js
+++
@@ -1,8 +0,0 @@
- 	var slider = $('.js-sliderCarouselWishlist').bxSlider({
-        minSlides: 4,
-        maxSlides: 4,
-        slideWidth: 199,
-        slideMargin: 2,
-        moveSlides: 1,
-        pager: false
-    });
